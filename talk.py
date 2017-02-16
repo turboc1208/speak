2e8345f8bd4da7a2fba492a6bef7f0cd079aafac
turboc1208 <chip.cox@coxdogs.com>
2017-02-15 20:26:55 -0600

    initial commit of talk.py

---
+++ talk.py
@@ -0,0 +1,14 @@
+import appdaemon.appapi as appapi
+            
+class talk(appapi.AppDaemon):
+
+  def initialize(self):
+    self.log("about to fire SPEAK event")
+    self.run_in(self.say_something,2)
+
+  def say_something(self, kwargs):
+    priority="1"
+    lang = "en"
+    #speaktext= "ahlexa, Turn on den fan light"
+    speaktext="I am alive   !!!"
+    self.fire_event("SPEAK_EVENT",text=speaktext, priority=priority,language=lang)
